9023a8552ebf1f6f8cf9e789cc2c1da812d35a71
rafael_bispo23@hotmail.com <rafael_bispo23@hotmail.com>
2021-06-07 20:37:42 -0300

    secão transição

--- _scripts/script.js
+++ _scripts/script.js
@@ -1,1 +1,39 @@
-alert("oi testando")
+ var quantidade = $('div#testemunhos ul').length;
+ var limite  = (quantidade * 336) * -1;
+ var largura  = limite +336;
+ var view_largura = $('section#testemunha div#testemunha-centro').width();
+ $('div#testemunhos').css('width',limite*-1);
+ $('div#teste').html(limite);
+
+ $('div#testemunha-direita').click(function(){
+   posicao_atual = $('div#testemunhos').position().left;
+   nova_posicao = posicao_atual - view_largura;
+  if (limite < posicao_atual - view_largura){
+    $('div#testemunhos').animate({left: nova_posicao},1000);
+  }
+
+  if( limite > (nova_posicao - view_largura)){
+    $('div#testemunha-direita ion-icon').css('opacity', 0.5);
+  $('div#testemunha-direita ion-icon').css('cursor', 'not-allowed');
+
+  }
+
+  $('div#testemunha-esquerda ion-icon').css('opacity', 1);
+  $('div#testemunha-esquerda ion-icon').css('cursor', 'pointer');
+  
+ })
+
+ $('div#testemunha-esquerda').click(function(){
+  posicao_atual = $('div#testemunhos').position().left;
+  if( posicao_atual + view_largura <= 0) {
+    nova_posicao = posicao_atual + view_largura;
+    $('div#testemunhos').animate({left: nova_posicao},1000);
+  }
+
+  if(nova_posicao == 0 ){
+    $('div#testemunha-esquerda ion-icon').css('opacity', 0.5);
+    $('div#testemunha-esquerda ion-icon').css('cursor', 'not-allowed');
+  }
+  $('div#testemunha-direita ion-icon').css('opacity', 1);
+  $('div#testemunha-direita ion-icon').css('cursor', 'pointer');
+})
